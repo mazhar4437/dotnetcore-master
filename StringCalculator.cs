d01e4c234ff12f4f5748fa749b13e4282c773906
Mazhar Mehdi <53686467+mazhar4437@users.noreply.github.com>
2021-03-08 00:17:54 +0500

    Updated Test Files

---
+++ StringCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dotnetcore
+{
+    class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            int result = 0;
+           
+            if(numbers.Length == 0)
+            {
+                 result = 0;
+            }
+           
+            else if(!numbers.Contains(',') && !numbers.Contains('\n'))
+            {
+                result = Convert.ToInt32(numbers);
+                
+            }
+            
+            else
+            {
+
+                string[] number = numbers.Split('\n',',');
+                foreach (var num in number)
+                {
+                    result += Convert.ToInt32(num);
+                }
+             }
+
+            
+            return result;
+        }
+    }
+}
